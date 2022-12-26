455b39f44182269c1fbe729846200728f35b1587
Zeel Gondaliya <zeelg@meditab.local>
2022-12-26 16:37:06 +0530

    new

--- index.js
+++ index.js
@@ -24,73 +24,23 @@ function submitData(event) {
 
 document.getElementById("formfinal").reset();
 
-// function patientOtherDetailShowFun() {
-//   let patientOtherDetailView = document.getElementsByClassName("PDCCODbottom")[0];
-//   let patientOtherDetailShowElement = document.getElementById("patientOtherDetailShow");
-
-//   if (patientOtherDetailShowElement.classList.contains("fa-circle-chevron-right")) {
-//       patientOtherDetailShowElement.classList.remove("fa-circle-chevron-right");
-//       patientOtherDetailShowElement.classList.add("fa-circle-chevron-down");
-//       patientOtherDetailView.style.display = "flex";
-//   } else {
-//       patientOtherDetailShowElement.classList.remove("fa-circle-chevron-down");
-//       patientOtherDetailShowElement.classList.add("fa-circle-chevron-right");
-//       patientOtherDetailView.style.display = "none";
-//   }
-// }
-
-// function patientLeftbarShowFun(){
-//   let PDCLeftbarShowElement = document.getElementById("PDCLeftbarShow");
-//   let PDCLeftbarView = document.getElementById("PDCLeftbar");
-//   let PDCLeftListIteamsView = document.getElementsByClassName("PDCLeftList")[0];
-
-//   if (PDCLeftbarShowElement.classList.contains("fa-chevron-left")) {
-//       PDCLeftbarShowElement.classList.remove("fa-chevron-left");
-//       PDCLeftbarShowElement.classList.add("fa-chevron-right");
-//       PDCLeftbarView.style.width = "17px";
-//       PDCLeftbarView.style.padding = "5px 0px 10px 0px";
-//       PDCLeftListIteamsView.style.color = "#e0e0e0";
-//   } else {
-//       PDCLeftbarShowElement.classList.remove("fa-chevron-right");
-//       PDCLeftbarShowElement.classList.add("fa-chevron-left");
-//       PDCLeftbarView.style.width = "220px";
-//       PDCLeftbarView.style.padding = "5px 15px 10px 17px";
-//       PDCLeftListIteamsView.style.color = "inherit";
-//   }
-
-// }
-
-// function submitPatientData(event) {
-//   event.preventDefault();
-
-//   let data = new FormData(document.getElementById("inputPatientData"));
-//   // console.log(data)
-
-//   let patientData = Object.create({})
-//   for (let [k, v] of data.entries()) {
-//       patientData[k] = v;
-//       // console.log(k," : ", v); 
-//   }
-
-//   console.log(patientData)
-
-//   const dob = new Date(patientData.dob);
-//   let month_diff = Date.now() - dob.getTime();
-//   let age_dt = new Date(month_diff);
-//   let year = age_dt.getUTCFullYear();
-//   let age = year - 1970;
-
-//   if (age < 0) {
-//       window.alert(`DOB can not be greater than today's date!`)
-//   }
-//   else if (age < 18) {
-//       window.alert(`Please add a contact for the Patient as ${patientData.gender == "Male" ? "he" : "she"}, is a minor.`)
-//   }
-
-  // console.log(age)
-//}
 
 
+var leftshowcontent = document.getElementById("sideshow");
+var sidebarCollapse = document.getElementById("sidebar-collapse");
+var expand = document.getElementById("maindiv");
+function sidebarShow(){
+  // console.log(leftshowcontent);
+  leftshowcontent.classList.toggle("hide-sidebar");
+  expand.classList.toggle("expand-main");
+}
+
+var othershowcontent = document.getElementById("other-in");
+var otherCollapse = document.getElementById("othersection");
+function otherInfoShow(){
+  // console.log(othershowcontent);
+  othershowcontent.classList.toggle("hide-otherinfo");
+}
 // "1. Collapsible SIdebar: Good
 // 2. Respoinsive: Good
 // 3. Scroll Handled and custom scroll style as per layout
@@ -216,3 +166,42 @@ document.getElementById("formfinal").reset();
 // 2. Do not use id for styling
 // 3. Need to resubmit again with styling and complete JS functionality
 // 4. Not complete form data is logged"
+
+// function patientLeftbarShowFun(){
+//   let PDCLeftbarShowElement = document.getElementById("PDCLeftbarShow");
+//   let PDCLeftbarView = document.getElementById("PDCLeftbar");
+//   let PDCLeftListIteamsView = document.getElementsByClassName("PDCLeftList")[0];
+
+//   if (PDCLeftbarShowElement.classList.contains("fa-chevron-left")) {
+//       PDCLeftbarShowElement.classList.remove("fa-chevron-left");
+//       PDCLeftbarShowElement.classList.add("fa-chevron-right");
+//       PDCLeftbarView.style.width = "17px";
+//       PDCLeftbarView.style.padding = "5px 0px 10px 0px";
+//       PDCLeftListIteamsView.style.color = "#e0e0e0";
+//   } else {
+//       PDCLeftbarShowElement.classList.remove("fa-chevron-right");
+//       PDCLeftbarShowElement.classList.add("fa-chevron-left");
+//       PDCLeftbarView.style.width = "220px";
+//       PDCLeftbarView.style.padding = "5px 15px 10px 17px";
+//       PDCLeftListIteamsView.style.color = "inherit";
+//   }
+
+// }
+
+
+  // // function otherInOut() {
+  // //   let patientOtherDetailView = document.getElementsByClassName("other-in")[0];
+  // //   let patientOtherDetailShowElement = document.getElementById("other-out");
+  
+  // //   if (patientOtherDetailShowElement.classList.contains("fa-solid fa-circle-plus")) {
+  // //       patientOtherDetailShowElement.classList.remove("fa-solid fa-circle-plus");
+  // //       patientOtherDetailShowElement.classList.add("fa-circle-chevron-down");
+  // //       patientOtherDetailView.style.display = "flex";
+  // //   } else {
+  // //       patientOtherDetailShowElement.classList.remove("fa-circle-chevron-down");
+  // //       patientOtherDetailShowElement.classList.add("fa-solid fa-circle-plus");
+  // //       patientOtherDetailView.style.display = "none";
+  // //   }
+  // // }
+  
+  
